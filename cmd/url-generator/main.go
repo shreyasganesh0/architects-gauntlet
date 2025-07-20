0ce52cfccd7d0ddbe03d50ff006774229d0fa84c
ArchWayland <vansi.ganeshs@gmail.com>
2025-07-19 23:28:33 -0400

    feat(lambda): update the function to responsed with a http gateway response
    - Update the functions response to send a HTTP response
    - Set a status code and sample url in the format
    - Marshal a url struct that sets the base that can be updated for future reponses

--- cmd/url-generator/main.go
+++ cmd/url-generator/main.go
@@ -3,15 +3,50 @@ package main
 import (
 	"log"
 	"context"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/events"
 )
 
-func handlerFunc(ctx context.Context, r events.APIGatewayV2HTTPRequest) (string, error) {
+type SuccessResponse struct {
+	URL string `json:"url"`
+}
 
+func handlerFunc(ctx context.Context, r events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 	log.Printf("Recieved event from HTTP API Gateway: %+v\n", r); 
+
+
+	response := SuccessResponse {
+		URL: "https://sample.com/example",
+	}
+
+	resp_byts, err := json.Marshal(response)
+	if err != nil {
+
+		return events.APIGatewayV2HTTPResponse{
+
+			StatusCode: 500,
+
+			Headers: map[string]string{
+				"Content-Type": "plain/text",
+			},
+
+			Body: "server error",
+		}, err
+	}
+
 	
-	return "Request recieved and logged", nil
+	return events.APIGatewayV2HTTPResponse {
+
+		StatusCode: 200,
+
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+
+		Body: string(resp_byts),
+
+	}, nil
 }
 
 func main() {
