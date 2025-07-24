ce3cdaa0c044a2fb5c84a862be59cab399718d93
ArchWayland <vansi.ganeshs@gmail.com>
2025-07-23 23:53:14 -0400

    feat(infra): create rds postgres instance
    - Adds aws_db_instance
    - link it to the networking data sources/subnet groups
    - placed in private subnets and configured to use password from secrets manager

--- database.tf
+++ database.tf
@@ -33,4 +33,38 @@ resource "aws_secretsmanager_secret_version" "database_password_version" {
 	secret_string = random_password.db_pass.result
 }
 
+data "aws_vpc" "def_vpc" {
+	default = true
+}
 
+data "aws_subnets" "def_subnet" {
+
+	filter {
+		name   = "vpc-id"
+		values = [data.aws_vpc.def_vpc.id]
+	}
+}
+
+resource "aws_db_subnet_group" "default" {
+	name = "main"
+	subnet_ids = data.aws_subnets.def_subnet.ids
+}
+
+resource "aws_db_instance" "db_instance" {
+	
+	allocated_storage = 20
+	instance_class = "db.t3.micro"
+	
+	engine = "postgres"
+	engine_version = "17.5"
+
+	db_subnet_group_name = aws_db_subnet_group.default.name
+
+	vpc_security_group_ids = [aws_security_group.database_sg.id]
+	
+	username = "masteruser"
+	password = random_password.db_pass.result
+
+	skip_final_snapshot = true
+	publicly_accessible = false
+}
